2499d955760b95030dc5a8c2f85811f39ede95b5
christinanikolova <christina.nikolova@abv.bg>
2021-01-12 18:14:41 +0200

    working on user personal data

---
+++ Web/CakeShop.Web/Areas/Identity/Pages/Account/InputModels/IndexInputModel.cs
@@ -0,0 +1,31 @@
+﻿namespace CakeShop.Web.Areas.Identity.Pages.Account.InputModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using CakeShop.Common;
+
+    public class IndexInputModel
+    {
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(DataValidation.UserFirstNameMaxLenght, ErrorMessage = GlobalConstants.ErrorMessage, MinimumLength = DataValidation.UserFirstNameMinLenght)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(DataValidation.UserLastNameMaxLenght, ErrorMessage = GlobalConstants.ErrorMessage, MinimumLength = DataValidation.UserLastNameMinLenght)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(DataValidation.UserAddressMaxLenght, ErrorMessage = GlobalConstants.ErrorMessage, MinimumLength = DataValidation.UserAddressMinLenght)]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Phone")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Web/CakeShop.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,97 @@
+﻿namespace CakeShop.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System.Threading.Tasks;
+
+    using CakeShop.Data.Models;
+    using CakeShop.Web.Areas.Identity.Pages.Account.InputModels;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public IndexInputModel Input { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            await this.LoadAsync(user);
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                await this.LoadAsync(user);
+
+                return this.Page();
+            }
+
+            var phoneNumber = await this.userManager.GetPhoneNumberAsync(user);
+
+            if (this.Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await this.userManager.SetPhoneNumberAsync(user, this.Input.PhoneNumber);
+
+                if (!setPhoneResult.Succeeded)
+                {
+                    this.StatusMessage = "Unexpected error when trying to set phone number.";
+                    return this.RedirectToPage();
+                }
+            }
+
+            await this.signInManager.RefreshSignInAsync(user);
+            this.StatusMessage = "Your profile has been updated";
+
+            return this.RedirectToPage();
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await this.userManager.GetUserNameAsync(user);
+            var phoneNumber = await this.userManager.GetPhoneNumberAsync(user);
+
+            this.Username = userName;
+
+            this.Input = new IndexInputModel
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Address = user.Address,
+                PhoneNumber = phoneNumber,
+            };
+        }
+    }
+}
