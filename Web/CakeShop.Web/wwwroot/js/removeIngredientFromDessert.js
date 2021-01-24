65f038a1d45fd425aba2d3a43273e6270c41edf6
christinanikolova <christina.nikolova@abv.bg>
2021-01-24 17:45:20 +0200

    fix bug with remove ingredient from dessert

---
+++ Web/CakeShop.Web/wwwroot/js/removeIngredientFromDessert.js
@@ -0,0 +1,29 @@
+﻿function removeIngredientFromDessert(event) {
+    if (event === undefined) {
+        return;
+    }
+
+    var ingredientName = event.target.parentElement.parentElement.innerText;
+    var dessertId = document.getElementById("dessert-id").innerHTML;
+
+    var token = $("#form-delete-ingredient input[name=__RequestVerificationToken]").val();
+
+    $.ajax({
+        url: "/Administration/Desserts/RemoveIngredientFromDessert",
+        type: "POST",
+        data: JSON.stringify({ ingredientName, dessertId }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: { 'X-CSRF-TOKEN': token },
+        success: function (data) {
+            var result = "";
+
+            data.dessertIngredients.forEach((element) => {
+                result += `<li class="remove-ingredient" onclick="removeIngredientFromDessert(event)">${element.ingredientName}<span class="cart__close"><span class="icon_close"></span></span>
+                            </li>`
+            });
+
+            $('#ingredients-list').html(result);
+        }
+    });
+};
