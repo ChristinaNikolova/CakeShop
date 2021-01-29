6bf164b238ba871e0eccbcaac012df6ae48b4e2d
christinanikolova <christina.nikolova@abv.bg>
2021-01-29 16:25:14 +0200

    refactor

---
+++ Web/CakeShop.Web/wwwroot/js/loadCommentAddForm.js
@@ -0,0 +1,13 @@
+﻿function loadCommentAddForm() {
+    document.getElementById("show-comment-form").addEventListener("click", showCommentForm);
+
+    function showCommentForm() {
+        let commentForm = document.getElementById("comment-form");
+
+        if (commentForm.style.display === "none") {
+            commentForm.style.display = "block";
+        } else {
+            commentForm.style.display = "none";
+        }
+    }
+}
